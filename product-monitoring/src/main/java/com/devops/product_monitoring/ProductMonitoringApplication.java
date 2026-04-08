c80fd0a939a4580c930e678ae57d3c7fbf03c6f9
Davi Gabriel <davi.fadriano@gmail.com>
2026-04-08 19:57:30 -0300

    feat: Estrutura inicial do projeto

---
+++ product-monitoring/src/main/java/com/devops/product_monitoring/ProductMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.devops.product_monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductMonitoringApplication.class, args);
+	}
+
+}
---
+++ product-monitoring/src/test/java/com/devops/product_monitoring/ProductMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.product_monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
