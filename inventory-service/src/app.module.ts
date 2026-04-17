37c6c150cbbf666e3ec5d1cb1a7b9aafc49d10d4
Dan Ruan <danrsaquetti@gmail.com>
2026-04-16 21:26:43 -0300

    feat: Criando estrutura de exeções com filter global

--- inventory-service/src/app.module.ts
+++ inventory-service/src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common'
+import { APP_FILTER } from '@nestjs/core'
 
 import { AppController } from '@presentation/controllers/app.controller'
+import { CatchAllExceptionFilter } from '@presentation/filters/catch-all.filter'
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: CatchAllExceptionFilter
+    }
+  ],
 })
 export class AppModule { }
