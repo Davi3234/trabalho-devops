1fe1a4265834a4047ee1c54a3e5073bd451ce4e9
Dan Ruan <danrsaquetti@gmail.com>
2026-04-18 20:26:29 -0300

    feat: Criando entidade de Produto

---
+++ inventory-service/src/domain/entities/produto.entity.ts
@@ -0,0 +1,90 @@
+import { EstoqueProduto } from '@domain/value-objects/estoque-produto.vo'
+import { ProdutoId } from '@domain/value-objects/produto-id.vo'
+import { BusinessException } from '@shared/exceptions/business.exception'
+
+export const NIVEL_CRITICO_THRESHOLD = 5
+
+export interface ProdutoProps {
+  id: ProdutoId
+  nome: string
+  quantidadeTotal: EstoqueProduto
+  quantidadeReservada: EstoqueProduto
+  version: number
+  criadoEm: Date
+  atualizadoEm: Date
+}
+
+export interface ProdutoCreateArgs {
+  id?: ProdutoId
+  nome: string
+  quantidadeTotal: EstoqueProduto
+  version?: number
+}
+
+export class Produto {
+
+  private readonly props: ProdutoProps
+
+  get id() { return this.props.id }
+  get nome() { return this.props.nome }
+
+  get quantidadeTotal() { return this.props.quantidadeTotal }
+  get quantidadeReservada() { return this.props.quantidadeReservada }
+  get quantidadeDisponivel() { return this.props.quantidadeTotal.subtract(this.props.quantidadeReservada) }
+
+  get criadoEm() { return this.props.criadoEm }
+  get atualizadoEm() { return this.props.atualizadoEm }
+
+  get version() { return this.props.version }
+
+  private constructor(props: ProdutoProps) {
+    this.props = props
+  }
+
+  static create(args: ProdutoCreateArgs) {
+    return new Produto({
+      id: args.id ?? ProdutoId.create(),
+      nome: args.nome,
+      quantidadeTotal: args.quantidadeTotal,
+      quantidadeReservada: EstoqueProduto.zero(),
+      version: args.version ?? 0,
+      criadoEm: new Date(),
+      atualizadoEm: new Date(),
+    })
+  }
+
+  static reconstitute(props: ProdutoProps) {
+    return new Produto(props)
+  }
+
+  hasDisponivel(quantidade: EstoqueProduto) {
+    return this.quantidadeDisponivel.isGreaterThanOrEqualTo(quantidade)
+  }
+
+  isNivelCritico() {
+    return this.quantidadeDisponivel.quantidade < NIVEL_CRITICO_THRESHOLD
+  }
+
+  reservar(quantidade: EstoqueProduto) {
+    if (!this.hasDisponivel(quantidade)) {
+      throw new BusinessException(
+        `Estoque insuficiente para produto ${this.props.id.id}. ` +
+        `Disponível: ${this.quantidadeDisponivel.quantidade}, solicitado: ${quantidade.quantidade}`,
+      )
+    }
+
+    this.props.quantidadeReservada = this.props.quantidadeReservada.add(quantidade)
+    this.props.atualizadoEm = new Date()
+  }
+
+  confirmarBaixa(quantidade: EstoqueProduto) {
+    this.props.quantidadeReservada = this.props.quantidadeReservada.subtract(quantidade)
+    this.props.quantidadeTotal = this.props.quantidadeTotal.subtract(quantidade)
+    this.props.atualizadoEm = new Date()
+  }
+
+  estornarReserva(quantidade: EstoqueProduto) {
+    this.props.quantidadeReservada = this.props.quantidadeReservada.subtract(quantidade)
+    this.props.atualizadoEm = new Date()
+  }
+}
