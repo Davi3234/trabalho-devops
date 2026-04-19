125d0338e6ac559c9360bccb7f10ce9198964d00
Dan Ruan <danrsaquetti@gmail.com>
2026-04-18 21:04:50 -0300

    refactor: Movendo cada validação de status da entidade Reserva para uma implementação de `ReservaState` independente

--- inventory-service/src/domain/state/reserva-status/reserva-status-fabric.ts
+++ inventory-service/src/domain/state/reserva-status/reserva-status-fabric.ts
@@ -1,9 +1,24 @@
 import { StatusReserva } from '@domain/enums/status-reserva.enum'
+import { ConfirmadoReservaState } from '@domain/state/reserva-status/confirmado-reserva.state'
+import { EstornadoReservaState } from '@domain/state/reserva-status/estornado-reserva.state'
+import { ExpiradoReservaState } from '@domain/state/reserva-status/expirado-reserva.state'
+import { PendenteReservaState } from '@domain/state/reserva-status/pendente-reserva.state'
 import { BusinessException } from '@shared/exceptions/business.exception'
 
 export class ReservaStatusFabric {
 
   static create(status: StatusReserva) {
-    throw new BusinessException(`Status de reserva inválido: ${status as string}`)
+    switch (status) {
+      case StatusReserva.PENDENTE:
+        return new PendenteReservaState()
+      case StatusReserva.CONFIRMADO:
+        return new ConfirmadoReservaState()
+      case StatusReserva.ESTORNADO:
+        return new EstornadoReservaState()
+      case StatusReserva.EXPIRADO:
+        return new ExpiradoReservaState()
+      default:
+        throw new BusinessException(`Status de reserva inválido: ${status as string}`)
+    }
   }
 }
