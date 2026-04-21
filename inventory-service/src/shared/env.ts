dc56adc95422e6942ce84e547fdccdf094d8d646
Dan Ruan <danrsaquetti@gmail.com>
2026-04-20 22:22:56 -0300

    config: Configurando Redis para uso de cache

--- inventory-service/src/shared/env.ts
+++ inventory-service/src/shared/env.ts
@@ -1,6 +1,10 @@
 export interface Environment {
   ENVIRONMENT: 'PRODUCTION' | 'DEVELOPMENT' | 'TEST'
   PORT: number
+  REDIS_HOST: string
+  REDIS_PORT: number
+  REDIS_PASSWORD: string
+  RABBITMQ_URL: string
 }
 
 export function env<T extends keyof Environment>(name: T, defaultValue?: Environment[T]) {
