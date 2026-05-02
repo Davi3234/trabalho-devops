5e01b1f3808350e281b02e53dbfc46442b04094b
Dan Ruan <danrsaquetti@gmail.com>
2026-05-02 10:31:34 -0300

    feat: Alterando registro do microsserviço RabbitMQ do NestJS para usar Golevelup/Nestjs

--- inventory-service/src/main.ts
+++ inventory-service/src/main.ts
@@ -1,5 +1,4 @@
 import { NestFactory } from '@nestjs/core'
-import { MicroserviceOptions, Transport } from '@nestjs/microservices'
 
 import { AppModule } from '@app.module'
 import { env } from '@shared/env'
@@ -7,17 +6,6 @@ import { env } from '@shared/env'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn', 'fatal', 'log'],
-  })
-
-  app.connectMicroservice<MicroserviceOptions>({
-    transport: Transport.RMQ,
-    options: {
-      urls: [env('RABBITMQ_URL', 'amqp://localhost')],
-      queue: 'estoque_consumer',
-      queueOptions: { durable: true },
-      noAck: false,
-      prefetchCount: 10,
-    }
   })
 
   await app.listen(env('PORT', 3000))
