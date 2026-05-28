d56ce17700b10294fc234164b44e4a560684bbdf
Dan Ruan <danrsaquetti@gmail.com>
2026-05-27 21:29:27 -0300

    feat: Ajustando para só liberar carregar o swagger apenas se o ambiente de execução for "DEVELOPMENT"

--- inventory-service/src/main.ts
+++ inventory-service/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 import { AppModule } from '@app.module'
+import { INestApplication } from '@nestjs/common'
 import { env } from '@shared/env'
 
 async function bootstrap() {
@@ -9,6 +10,14 @@ async function bootstrap() {
     logger: ['error', 'warn', 'fatal', 'log'],
   })
 
+  if (env('ENVIRONMENT', 'DEVELOPMENT') == 'DEVELOPMENT') {
+    loadSwagger(app)
+  }
+
+  await app.listen(env('PORT', 3000))
+}
+
+function loadSwagger(app: INestApplication) {
   const swaggerConfig = new DocumentBuilder()
     .setTitle('Inventory Service')
     .setDescription('API de controle de estoque - gerenciamento de produtos, reservas e entradas de estoque')
@@ -19,8 +28,6 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, swaggerConfig)
   SwaggerModule.setup('docs', app, document)
-
-  await app.listen(env('PORT', 3000))
 }
 
 bootstrap()
