a4b25cc4693636f346bfaa989ef43ceaf8084acf
Dan Ruan <danrsaquetti@gmail.com>
2026-05-30 14:49:29 -0300

    feat: Criando implementação de serviço de cache em memória

--- inventory-service/src/infrastructure/redis.module.ts
+++ inventory-service/src/infrastructure/redis.module.ts
@@ -2,13 +2,13 @@ import { Module } from '@nestjs/common'
 
 import { LOCK_SERVICE_TOKEN } from '@application/ports/lock-service.port'
 import { CacheService } from '@infrastructure/cache/cache-service'
-import { RedisCacheService } from '@infrastructure/cache/implementations/redis-cache.service'
+import { InMemoryCacheService } from '@infrastructure/cache/implementations/in-memory-cache.service'
 import { RedisLockService } from '@infrastructure/ports/redis-lock.service'
 import { REDIS_CLIENT_TOKEN } from '@infrastructure/redis.token'
 
 @Module({
   providers: [
-    RedisCacheService,
+    InMemoryCacheService,
     RedisLockService,
     {
       provide: LOCK_SERVICE_TOKEN,
@@ -16,9 +16,9 @@ import { REDIS_CLIENT_TOKEN } from '@infrastructure/redis.token'
     },
     {
       provide: CacheService,
-      useClass: RedisCacheService,
+      useClass: InMemoryCacheService,
     },
   ],
-  exports: [REDIS_CLIENT_TOKEN, RedisLockService, CacheService, RedisCacheService, LOCK_SERVICE_TOKEN],
+  exports: [REDIS_CLIENT_TOKEN, RedisLockService, CacheService, InMemoryCacheService, LOCK_SERVICE_TOKEN],
 })
 export class RedisModule { }
